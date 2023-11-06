1bd4415cc286207a9a3a718d8c1a55d71726c2ee
Azukimochi <Azukimochi25565@gmail.com>
2023-11-06 17:49:02 +0900

    first commit

---
+++ Editor/KintoneApp.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace io.github.azukimochi
+{
+    public class KintoneApp : EditorWindow
+    {
+        private static string KINTONE_USER = "";
+        private static string KINTONE_MAIL = "";
+        private static string KINTONE_API_URL = "";
+        private static string KINTONE_API_KEY = "";
+        
+        private Tab _tab = Tab.Main;
+        private bool _UpdateSolution = false;
+
+        private string projectName = "";
+        private string todoContetnt = "";
+
+        // Start is called before the first frame update
+        enum Tab
+        {
+            Main,
+            Settings,
+        }
+        
+        [MenuItem("Window/Kintone TodoApp")]
+        public static void ShowWindow()
+        {
+            GetWindow<KintoneApp>("Kintone TodoApp");
+        }
+        private void Update()
+        {
+            if(_UpdateSolution)
+            {
+                _UpdateSolution = false;
+                Repaint();
+            }
+        }
+        private void OnEnable (){
+            KINTONE_USER = Utils.DecryptAES(EditorPrefs.GetString("io.github.Azukimochi.KINTONE_USER", ""));
+            KINTONE_MAIL = Utils.DecryptAES(EditorPrefs.GetString("io.github.Azukimochi.KINTONE_MAIL", ""));
+            KINTONE_API_URL = Utils.DecryptAES(EditorPrefs.GetString("io.github.Azukimochi.KINTONE_API_URL", ""));
+            KINTONE_API_KEY = Utils.DecryptAES(EditorPrefs.GetString("io.github.Azukimochi.KINTONE_API_KEY", ""));
+        }
+        void OnGUI()
+        {
+            _tab = (Tab)GUILayout.Toolbar((int)_tab, Styles.TabToggles, Styles.TabButtonStyle, Styles.TabButtonSize);
+            
+            switch (_tab)
+            {
+                case Tab.Main:
+                    DrawMainTab();
+                    break;
+                case Tab.Settings:
+                    DrawSettingsTab();
+                    break;
+            }
+        }
+
+        public void DrawMainTab()
+        {
+            GUILayout.Label("ToDo List", EditorStyles.boldLabel);
+            GUILayout.Space(20);
+            
+            GUILayout.Label("プロジェクト名" , EditorStyles.boldLabel);
+            projectName = EditorGUILayout.TextField(projectName);
+            GUILayout.Label("ToDo" , EditorStyles.boldLabel);
+            todoContetnt = EditorGUILayout.TextArea(todoContetnt, GUILayout.Height(50));
+            if (GUILayout.Button("送信"))
+            {
+                kintoneAPIHundler.sendKintoneAPI(KINTONE_USER, KINTONE_MAIL, KINTONE_API_KEY, KINTONE_API_URL, projectName, todoContetnt);
+            }
+        }
+
+        public void DrawSettingsTab()
+        {
+            GUILayout.Label("kintone User Name" , EditorStyles.boldLabel);
+            KINTONE_USER = EditorGUILayout.TextArea(KINTONE_USER);
+            GUILayout.Label("kintone Mail" , EditorStyles.boldLabel);
+            KINTONE_MAIL = EditorGUILayout.TextArea(KINTONE_MAIL);
+            GUILayout.Label("kintone API URL" , EditorStyles.boldLabel);
+            KINTONE_API_URL = EditorGUILayout.PasswordField(KINTONE_API_URL);
+            GUILayout.Label("kintone API Key" , EditorStyles.boldLabel);
+            KINTONE_API_KEY = EditorGUILayout.PasswordField(KINTONE_API_KEY);
+            GUILayout.Space(20);
+            
+            if (GUILayout.Button("適用"))
+            {
+                Debug.Log("save key and url");
+                EditorPrefs.SetString("io.github.Azukimochi.KINTONE_USER", Utils.EncryptAES(KINTONE_USER));
+                EditorPrefs.SetString("io.github.Azukimochi.KINTONE_MAIL", Utils.EncryptAES(KINTONE_MAIL));
+                EditorPrefs.SetString("io.github.Azukimochi.KINTONE_API_KEY", Utils.EncryptAES(KINTONE_API_KEY));
+                EditorPrefs.SetString("io.github.Azukimochi.KINTONE_API_URL", Utils.EncryptAES(KINTONE_API_URL));
+            }
+        }
+        private static class Styles
+        {
+            private static GUIContent[] _tabToggles = null;
+            public static GUIContent[] TabToggles{
+                get {
+                    if (_tabToggles == null) {
+                        _tabToggles = System.Enum.GetNames(typeof(Tab)).Select(x => new GUIContent(x)).ToArray();
+                    }
+                    return _tabToggles;
+                }
+            }
+            public static readonly GUIStyle TabButtonStyle = "LargeButton";
+            public static readonly GUI.ToolbarButtonSize TabButtonSize = GUI.ToolbarButtonSize.Fixed;
+        }
+        
+    }
+}
---
+++ Editor/kintoneAPIHandler.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using UnityEditor;
+using Newtonsoft.Json.Linq;
+
+public class kintoneAPIHundler : MonoBehaviour
+{
+
+    public static void sendKintoneAPI(string USER, string MAIL, string API_KEY, string API_URL, string projectName, string todoContent)
+    {
+        var parameters = new Dictionary<string, object>()
+        {
+            { "app", 1 },
+            {
+                "record", new Dictionary<string, object>()
+                {
+                    {"str_projectName", new Dictionary<string, string>()
+                        {
+                            {"value", projectName}
+                        } 
+                    },
+                    {"str_toDo", new Dictionary<string, string>()
+                        {
+                            {"value", todoContent}
+                        } 
+                    },
+                    {"更新者", new Dictionary<string, object>()
+                        {
+                            {"type", "MODIFIER"},
+                            {"value", new Dictionary<string, string>()
+                                {
+                                    {"code", MAIL},
+                                    {"name", USER}
+                                }
+                            }
+                        }
+                    },
+                    {"作成者", new Dictionary<string, object>()
+                        {
+                            {"type", "CREATOR"},
+                            {"value", new Dictionary<string, string>()
+                                {
+                                    {"code", MAIL},
+                                    {"name", USER}
+                                }
+                            }
+                        }
+                    }
+                }
+            },
+            
+        };
+        using (var httpClient = new HttpClient())
+        {
+            string json = JsonConvert.SerializeObject(parameters);
+            
+            var content  = new StringContent(json, Encoding.UTF8, "application/json");
+            //content.Headers.ContentType = new MediaTypeHeaderValue("application/x-www-form-urlencoded");
+            //Debug.Log("apikey:" + API_KEY);
+            //Debug.Log("apiurl:" + API_URL);
+            //Debug.Log(json);
+            httpClient.DefaultRequestHeaders.Add(@"X-Cybozu-API-Token", API_KEY);
+            var response = httpClient.PostAsync(API_URL,content).Result;
+            
+            var responseBody = response.Content.ReadAsStringAsync().Result;
+            var jsonResponse = JObject.Parse(responseBody);
+            Debug.Log(jsonResponse.ToString());
+        }
+    }
+}
